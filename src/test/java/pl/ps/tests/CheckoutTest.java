7a2aa20e029815c3940c7717aa31c4ff40cdb2ba
PiotrS97 <piotr.szatkowski97@gmail.com>
2023-03-28 19:19:57 +0200

    Add CheckoutTest and some Page classes

---
+++ src/test/java/pl/ps/tests/CheckoutTest.java
@@ -0,0 +1,18 @@
+package pl.ps.tests;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pl.ps.pages.HomePage;
+
+public class CheckoutTest extends BaseTest {
+    @Test
+    public void checkoutTest() {
+        new HomePage(driver)
+                .openShopPage()
+                .openProduct("Java Selenium WebDriver")
+                .addProductToCart()
+                .viewCart()
+                .openAddressDetails();
+    }
+}
